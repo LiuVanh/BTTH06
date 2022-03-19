1ffc32ec60bac1b275717296b953b9fda6a47e34
Luu Viet Anh <l.rivazh@gmail.com>
2022-03-19 23:53:03 +0700

    blink banner

--- JS/Yeucau3.js
+++ JS/Yeucau3.js
@@ -10,12 +10,17 @@ function CurTime() {
   var timer = `${hours}:${minutes}:${seconds}`;
   document.getElementById("time-banner").textContent = timer;
   if (hours >= 0 && hours < 12) {
-    document.getElementById("time-banner").textContent = "Chào buổi sáng";
+    document.getElementById("banner").textContent = "Chào buổi sáng";
   } else if (hours >= 12 && hours < 17) {
-    document.getElementById("time-banner").textContent = "Chào buổi chiều";
+    document.getElementById("banner").textContent = "Chào buổi chiều";
   }
   if (hours >= 17 && hours < 24)
     document.getElementById("banner").textContent = "Chào buổi tối";
 }
 setInterval(CurTime, 1000);
 
+function blink_banner() {
+  var element = document.getElementById("banner");
+  element.classList.toggle("blink_banner");
+}
+setInterval(blink_banner, 1000);
