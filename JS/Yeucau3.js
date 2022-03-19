48d0640056a185aea9add1fcff8bd574713d5899
LiuVanh <99171095+LiuVanh@users.noreply.github.com>
2022-03-19 22:39:54 +0700

    Update Yeucau3.js

--- JS/Yeucau3.js
+++ JS/Yeucau3.js
@@ -1,12 +1,12 @@
 function tg() {
   var d = new Date();
   var t = d.toLocaleTimeString();
-
   var hours = d.getHours();
-
+  if(hours<10){hours="0"+hours}
   var minutes = d.getMinutes();
-
+  if(minutes<10){minutes="0"+minutes}
   var seconds = d.getSeconds();
+  if(seconds<10){seconds="0"+seconds}
   var timer = `${hours}:${minutes}:${seconds}`;
   document.getElementById("time-banner").textContent = timer;
   if (hours >= 0 && hours < 12) {
